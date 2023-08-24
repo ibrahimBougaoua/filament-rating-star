defcabb855dc3ac7f1db7408b47371b1648bed5a
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2023-08-24 16:02:56 +0100

    Feat : Column rating stars for table

---
+++ src/Columns/RatingStarColumn.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace IbrahimBougaoua\FilamentRatingStar\Columns;
+
+use Filament\Tables\Columns\Column;
+use Filament\Forms\Components\Concerns\HasToggleColors;
+use Filament\Forms\Components\Concerns\HasToggleIcons;
+use Filament\Tables\Columns\Concerns\CanBeValidated;
+use Filament\Tables\Columns\Concerns\CanUpdateState;
+use Filament\Tables\Columns\Contracts\Editable;
+
+class RatingStarColumn extends Column implements Editable
+{
+    use CanBeValidated;
+    use CanUpdateState;
+    use HasToggleColors;
+    use HasToggleIcons;
+
+    protected string $view = 'filament-rating-star::columns.rating-star-column';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->disabledClick();
+
+        $this->rules(['boolean']);
+    }
+}
