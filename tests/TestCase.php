efaa5c356b0b3372eb96bea2f0d76af88785162e
ibrahimBougaoua <ibrahimBougaoua@users.noreply.github.com>
2024-01-29 10:50:02 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace IbrahimBougaoua\FilamentRatingStar\Tests;
 
+use IbrahimBougaoua\FilamentRatingStar\FilamentRatingStarServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use IbrahimBougaoua\FilamentRatingStar\FilamentRatingStarServiceProvider;
 
 class TestCase extends Orchestra
 {
