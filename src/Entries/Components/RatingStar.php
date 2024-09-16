e064b84f533e755578bfcda1dee157f5a47271e1
Lewis Raggett <46108903+redsquirrelstudio@users.noreply.github.com>
2024-09-16 10:34:24 +0100

    Updated view reference in entry

--- src/Entries/Components/RatingStar.php
+++ src/Entries/Components/RatingStar.php
@@ -5,16 +5,13 @@ namespace IbrahimBougaoua\FilamentRatingStar\Entries\Components;
 use Filament\Forms\Components\Concerns\HasToggleColors;
 use Filament\Forms\Components\Concerns\HasToggleIcons;
 use Filament\Infolists\Components\Entry;
-use Filament\Tables\Columns\Concerns\CanBeValidated;
-use Filament\Tables\Columns\Concerns\CanUpdateState;
-use Filament\Tables\Columns\Contracts\Editable;
 
 class RatingStar extends Entry
 {
     use HasToggleColors;
     use HasToggleIcons;
 
-    protected string $view = 'filament-rating-star::columns.components.rating-star';
+    protected string $view = 'filament-rating-star::entries.components.rating-star';
 
     protected string $size = 'lg';
 
